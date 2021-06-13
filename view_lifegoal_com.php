9b8610805de5ad5458f3401f698a5a59b3c26899
Hyerim Jun <21900648@handong.edu>
2021-06-13 13:59:43 -0900

    committed

---
+++ view_lifegoal_com.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>view</title>
+</head>
+<body>
+    <?php
+	$conn=mysqli_connect("localhost","2ill","cuby1103","list");
+        if(!$conn){
+            echo 'db에 연결하지 못했습니다'.mysqli_connect_error();
+	}
+
+	$num = $_GET['num'];
+	$sql = "SELECT * FROM achieve WHERE num = $num";
+       	$result = mysqli_query($conn,$sql);
+
+	$row = mysqli_fetch_array($result);
+	echo "<h1>{$row['title']}</h1>";
+	echo "<p style='color:grey'>create-date : {$row['create_date']}";
+	echo "&nbsp &nbsp  complete-date : {$row['complete_date']}</p>";
+
+	echo "<hr>";
+	echo "<p>{$row['description']}</p>";
+
+	mysqli_close($conn);
+    ?>
+
+    <br>
+    <a href="modify_lifegoal.php?num=<?= $row['num'] ?>">MODIFY</a>   
+    <a href="delete_lifegoal.php?num=<?= $row['num'] ?>">DELETE</a> <br><br>  
+    <a href="lifegoal.php">BACK TO LIST</a>   
+
+</body>
+</html>
---
+++ write_todo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>write</title>
+</head>
+<body>
+    <h1>ADD NEW</h1>
+    <form action = "insert_todo.php" method="POST">
+	<p>
+	    <label for="title">Title: </label>
+	    <input type="text" id="title" name="title"></input>
+	</p>
+	<p>
+	    <label for="descriptioin">Description: </label>
+	    <textarea name="description" id="description" cols="120" rows="20"></textarea>
+	</p>
+	<input type="submit" value="add">
+    </form>
+    <br>
+    <a href = "todo.php">cancel</a>
+</body>
+</html>
